d12dedcaaf12d52f289432f5124cb56754f1f620
github-Ocean-Soft <vanthinhbg2000@gmail.com>
2021-10-19 15:33:02 +0700

    del

--- database/Connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$conn = mysqli_connect("localhost", "root", "","php_dev");
-	if ($conn){
-	mysqli_query($conn,"SET NAMES 'utf8' ");
-	
-	}else{
-	echo "Bạn kết nối thất bại".mysqli_connect_error();
-	}
-?>
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-	include "Controllers/ProductController.php";
-	//include "View/user.php";
-	//include "View/userpost.php";
-?>
