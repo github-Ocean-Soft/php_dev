466ef8708a52f377f647e6b8a7079cbb14dd1aec
github-Ocean-Soft <vanthinhbg2000@gmail.com>
2021-10-20 22:54:49 +0700

    del

--- database/create_database.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-//kết nối đến mysql
-$link = mysqli_connect("localhost", "root", "");
-// Thực hiện tạo CSDL
-$sql = "CREATE DATABASE php_dev";
-mysqli_query($link, $sql);
-?>
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include "database/Connection.php";
-	include "Controllers/ProductController.php";
-	//include "View/user.php";
-	//include "View/userpost.php";
-?>
